a5b688ca514df85b45c65f3ab3fe2c391797d9ec
Ethan Guo <ethguo@gmail.com>
2016-08-01 19:49:17 -0400

    Better challenge handling?

--- index.js
+++ index.js
@@ -13,10 +13,17 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+// ACME challenge (sjamcs.club)
 app.get('/.well-known/acme-challenge/GWLE-kNm2bQ0f43Q-jwWwGlxDumDEzWjVZ-QdXjDJik', function(request, response) {
   response.send('GWLE-kNm2bQ0f43Q-jwWwGlxDumDEzWjVZ-QdXjDJik.tRZzhjhg1nDNW1U_Ts6YvwgYDgbT27gxEsQ-V1JPB8k');
 });
 
+// ACME challenge (www.sjamcs.club)
+app.get('/.well-known/acme-challenge/d78T6SA8OQSq7Nf8nXbNTDpmd741ehSDRmjWs4V4hxM', function(request, response) {
+  response.send('d78T6SA8OQSq7Nf8nXbNTDpmd741ehSDRmjWs4V4hxM.tRZzhjhg1nDNW1U_Ts6YvwgYDgbT27gxEsQ-V1JPB8k');
+});
+
+// New www challenge
 app.get('/.well-known/acme-challenge/XCQYJAmB9KlFyuKUvCFu-bAvGKrRsq5Sfj4EziEEnwQ', function(request, response) {
   response.send('XCQYJAmB9KlFyuKUvCFu-bAvGKrRsq5Sfj4EziEEnwQ.tRZzhjhg1nDNW1U_Ts6YvwgYDgbT27gxEsQ-V1JPB8k');
 });
