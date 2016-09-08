d932525ce577beedcf32ba9015c82ea4a60cdfe3
Ethan Guo <ethguo@gmail.com>
2016-09-07 20:00:49 -0400

    Redirect / to forums.sjamcs.club and removed all other routing

--- index.js
+++ index.js
@@ -11,33 +11,9 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-var client = new pg.Client();
-
-app.get('/db', function(request, response) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		if (err) throw err;
-		client.query("INSERT INTO test_table (name) VALUES ('hello postgres');", function(err, result) {
-			if (err) {
-				console.error(err);
-				response.send('Error: ' + err);
-			} else {
-				client.query('SELECT * FROM test_table', function(err, result) {
-					done();
-					if (err) {
-						console.error(err);
-						response.send('Error: ' + err);
-					} else {
-						response.render('pages/db', {results: result.rows});
-					}
-				});
-			}
-		});
-	});
+	response.redirect("https://forums.sjamcs.club/")
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
